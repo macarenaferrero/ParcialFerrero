df47ecc7fa5c891baa62d576ce1ded7ec54854e0
Macarena Ferrero <macarena.ferrero@soulit.io>
2023-05-17 15:38:59 -0300

    limpieza codigo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { SharedModule } from './shared/shared.module';
-import { ToastrModule } from 'ngx-toastr';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ActoresRoutingModule } from './actor/actores-routing.module';
-import { PeliculasRoutingModule } from './pelicula/peliculas-routing.module';
-import { HomeComponent } from './pages/home/home.component';
+import { NgModule } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { environment } from '../environments/environment';
+import { ActoresRoutingModule } from './actor/actores-routing.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PeliculasRoutingModule } from './pelicula/peliculas-routing.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
